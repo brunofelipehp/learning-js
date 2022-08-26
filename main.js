d064c1e9bf52c3c5cb8ef99330d298c751ec6cd3
bruno felipe <brunofelipehp8@gmail.com>
2022-08-26 13:01:44 -0300

    Aprendendo a utilizar ES modules

--- main.js
+++ main.js
@@ -1,56 +1,9 @@
-// map
+//import { soma, sub, PI } from './lib/math';
 
-const array = [1, 2, 3, 4, 5];
-//const novoArray =[];
+//console.log(soma(2, 5));
+//console.log(sub(8, 4));
+//console.log(PI);
 
-/*
-for (const item of array) {
-  document.body.innerText += item;
-}
-*/
+import * as math from './lib/math';
 
-//array.forEach(item => {
- // novoArray.push(item * 2)
-//});
-
-// map
-/*
-const novoArray = array.map(item => {
-  if(item % 2 === 0) { 
-    return item * 10
-   }
-
-   return item;
-})
-*/
-
-/* 
-
-//filter
-const novoArray = array
-.filter(item => item % 2 !== 0)
-.map(item => item * 10)
-
-*/
-
-/*
-const todosItems = array.every(item => {
- return typeof item !== 'number'
-})
-*/
-
-/*
-const peloMenosUm = array.some( item => {
-  return typeof item === 'number'
-})
-*/
-
-//find
-//const par = array.find(item => item % 2 === 0)
-//const index = array.findIndex(item => item % 2 === 0)
-
-const soma = array.reduce((acc, item) => {
-  return acc + item
-}, 0)
-
-document.body.innerText = JSON.stringify(soma);
+console.log(math);
